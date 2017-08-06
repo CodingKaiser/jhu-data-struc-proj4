26e7b3629b50cc0dc8db9892aab57c58ae427c5a
Falko Julian Noe <falkonoe@gmail.com>
2017-08-06 00:56:17 -0700

    Supports printing array contents and swapping elements.

---
+++ src/ArrayOperations.java
@@ -0,0 +1,23 @@
+public class ArrayOperations {
+
+  public static int[] makeCopy(int[] x) {
+    int[] copy = new int[x.length];
+    for (int i = 0; i < x.length; i++) {
+      copy[i] = x[i];
+    }
+    return copy;
+  }
+
+  public static void printContents(int[] x) {
+    for (int i = 0; i < x.length; i++) {
+      System.out.print(x[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void exchangeElements(int[] input, int i, int j) {
+    int temp = input[i];
+    input[i] = input[j];
+    input[j] = temp;
+  }
+}
